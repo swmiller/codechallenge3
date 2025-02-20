2afae4a8c329864db1d62eeb5ab6ec618ed43fcb
Stephen Miller <smiller@crowninformationservices.com>
2025-02-20 15:12:10 -0600

    Initial check in

---
+++ src/main/java/com/syf/codechallenge3/Codechallenge3Application.java
@@ -0,0 +1,13 @@
+package com.syf.codechallenge3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Codechallenge3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Codechallenge3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/syf/codechallenge3/Codechallenge3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syf.codechallenge3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Codechallenge3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
