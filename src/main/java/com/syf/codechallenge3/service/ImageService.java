d9e9d72b2e1e29040351be9782c9f541827a8683
Stephen Miller <smiller@crowninformationservices.com>
2025-02-22 16:59:59 -0600

    Added Imgur config to Image service

--- src/main/java/com/syf/codechallenge3/service/ImageService.java
+++ src/main/java/com/syf/codechallenge3/service/ImageService.java
@@ -2,15 +2,25 @@ package com.syf.codechallenge3.service;
 
 import org.springframework.stereotype.Service;
 
+import com.syf.codechallenge3.config.ImgurConfig;
 import com.syf.codechallenge3.model.ImageDto;
 import com.syf.codechallenge3.repository.ImageRepository;
 
 @Service
 public class ImageService {
     private final ImageRepository imageRepository;
+    private final ImgurConfig imgurConfig;
+    private final String imgurClientId;
+    private final String imgurClientSecret;
+    private final String imgurApiImageBaseUrl;
 
-    public ImageService(ImageRepository imageRepository) {
+    public ImageService(ImageRepository imageRepository, ImgurConfig imgurConfig) {
         this.imageRepository = imageRepository;
+        this.imgurConfig = imgurConfig;
+
+        this.imgurClientId = imgurConfig.getClientId();
+        this.imgurClientSecret = imgurConfig.getClientSecret();
+        this.imgurApiImageBaseUrl = imgurConfig.getImgurApiImageBaseUrl();
     }
 
     public void deleteImageById(long id) {
@@ -18,6 +28,7 @@ public class ImageService {
     }
 
     public ImageDto uploadImage(ImageDto imageDto) {
+
         // Save image logic
         return new ImageDto();
     }
