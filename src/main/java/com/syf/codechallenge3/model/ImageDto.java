507cf83fa7ae8c28ab4db533571bbabb65ffd936
Stephen Miller <smiller@crowninformationservices.com>
2025-02-24 15:14:15 -0600

    Converted ImageDto to use Lombok

--- src/main/java/com/syf/codechallenge3/model/ImageDto.java
+++ src/main/java/com/syf/codechallenge3/model/ImageDto.java
@@ -1,5 +1,13 @@
 package com.syf.codechallenge3.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class ImageDto {
     private String fileName;
     private String fileType; // Image or video
@@ -13,103 +21,6 @@ public class ImageDto {
     private String imgurDeleteHash;
     private String imgurLink;
     private String imgurId;
-
-    // Getters and Setters
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public String getFileType() {
-        return fileType;
-    }
-
-    public void setFileType(String fileType) {
-        this.fileType = fileType;
-    }
-
-    public byte[] getImageData() {
-        return imageData;
-    }
-
-    public void setImageData(byte[] imageData) {
-        this.imageData = imageData;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getImageFormat() {
-        return imageFormat;
-    }
-
-    public void setImageFormat(String imageFormat) {
-        this.imageFormat = imageFormat;
-    }
-
-    public String getVideoFormat() {
-        return videoFormat;
-    }
-
-    public void setVideoFormat(String videoFormat) {
-        this.videoFormat = videoFormat;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImgurDeleteHash() {
-        return imgurDeleteHash;
-    }
-
-    public void setImgurDeleteHash(String imgurImageHash) {
-        this.imgurDeleteHash = imgurImageHash;
-    }
-
-    public String getImgurLink() {
-        return imgurLink;
-    }
-
-    public void setImgurLink(String imgurLink) {
-        this.imgurLink = imgurLink;
-    }
-
-    public String getImgurId() {
-        return imgurId;
-    }
-
-    public void setImgurId(String imgurId) {
-        this.imgurId = imgurId;
-    }
 
     // Methods
 
